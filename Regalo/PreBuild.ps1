fec8ab052c4f2e905fd59e4ba5f6e3ea9a98a28c
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-02-28 00:42:23 +0000

    Automatic assembly versioning completed.

--- Regalo/PreBuild.ps1
+++ Regalo/PreBuild.ps1
@@ -1,2 +1,24 @@
-param($projectDir)
+param($assemblyInfoFilename)
 
+$version = git describe --tags --long
+$version = $version -replace "v", ""
+
+$matched = $version -match "^(\d)\.(\d)\.(\d)\-(\d)-([A-Za-z0-9]{8})$"
+
+$majorVersion = $Matches[1]
+$minorVersion = $Matches[2]
+$buildVersion = $Matches[3]
+$revisionVersion = $Matches[4]
+
+$version = "$majorVersion.$minorVersion.$buildVersion.$revisionVersion"
+
+write-host "Building output as $version..."
+
+$assemblyVersion = '[assembly: AssemblyVersion("' + $version + '")]'
+$assemblyFileVersion = '[assembly: AssemblyFileVersion("' + $version + '")]'
+
+
+"using System.Reflection;" | out-file $assemblyInfoFilename
+"" | out-file $assemblyInfoFilename -append
+$assemblyVersion | out-file $assemblyInfoFilename -append
+$assemblyFileVersion | out-file $assemblyInfoFilename -append
