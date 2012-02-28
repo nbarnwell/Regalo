7317d0fcfb2c823f2f637226fd9b15ca69a4f2fd
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-02-28 09:28:04 +0000

    Beginning implementation of Raven event store.

---
+++ Regalo.EventSourcing.Raven.Tests.Unit/PersistenceTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Raven.Client;
+using Raven.Client.Embedded;
+
+namespace Regalo.EventSourcing.Raven.Tests.Unit
+{
+    [TestFixture]
+    public class PersistenceTests
+    {
+        //[Test]
+        //public void Loading_GivenEmptyStore_ShouldReturnNull()
+        //{
+        //    // Arrange
+        //    IDocumentStore documentStore = new EmbeddableDocumentStore { RunInMemory = true };
+        //    IRepository<Customer> repository = new RavenRepository<Customer>(documentStore);
+
+        //    // Act
+        //    Customer customer = repository.Get("customer1");
+
+        //    // Assert
+        //    Assert.Null(customer);
+        //}
+    }
+}
