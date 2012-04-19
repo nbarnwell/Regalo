784c4b43fd06859776e6b777a72ca1826ddc7678
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-04-19 18:00:48 +0100

    Added ICommandProcessor to replace ICommandBus.

--- Regalo.Core/ICommandBus.cs
+++ Regalo.Core/ICommandBus.cs
@@ -1,5 +1,8 @@
-﻿namespace Regalo.Core
+﻿using System;
+
+namespace Regalo.Core
 {
+    [Obsolete("Please use ICommandProcessor instead", true)]
     public interface ICommandBus
     {
         void Send<TCommand>(TCommand command);
---
+++ Regalo.Core/ICommandProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace Regalo.Core
+{
+    public interface ICommandProcessor
+    {
+        void Process<TCommand>(TCommand command);
+    }
+}
