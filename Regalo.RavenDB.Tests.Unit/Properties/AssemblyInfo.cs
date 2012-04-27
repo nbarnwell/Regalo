31f5a3be517ab8a4c10755c368e61848c02d9abd
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-04-27 23:10:32 +0100

    Fixed package name for Regalo.RavenDB.

--- Regalo.RavenDB.Tests.Unit/Properties/AssemblyInfo.cs
+++ Regalo.RavenDB.Tests.Unit/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Regalo.EventSourcing.Raven.Tests.Unit")]
+[assembly: AssemblyTitle("Regalo.Raven.Tests.Unit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- Regalo.RavenDB/Properties/AssemblyInfo.cs
+++ Regalo.RavenDB/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Regalo.EventSourcing.Raven")]
+[assembly: AssemblyTitle("Regalo.Raven")]
 [assembly: AssemblyDescription("A RavenDB-based event store implementation for Regalo.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
