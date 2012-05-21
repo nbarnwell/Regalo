4a51d870dcac81d230cbdb7585218b62747b25a9
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-05-21 17:31:03 +0100

    Added automatic versioning to Regalo.Testing.

--- Regalo.Testing/Properties/AssemblyInfo.cs
+++ Regalo.Testing/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a5309140-71f9-40dc-95f8-0a79a3798741")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
