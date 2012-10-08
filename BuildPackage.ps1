40c2b8f35fc2a8a170122096fbe3cc51149322b6
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-10-08 17:32:12 +0100

    Fixed build error in Testing project resulting from duplicate AssemblyDescription assembly attributes.

--- BuildPackage.ps1
+++ BuildPackage.ps1
@@ -3,6 +3,6 @@ $outputDir = "$scriptDir\BuildOutput"
 
 md $outputDir -f
 
-gci $scriptDir -include Regalo.Core.csproj,Regalo.RavenDB.csproj -recurse | %{ 
+gci $scriptDir -include Regalo.Core.csproj,Regalo.RavenDB.csproj,Regalo.Testing.csproj -recurse | %{ 
     nuget.exe pack $_ -Build -Symbols -outputdirectory $outputDir -Properties Configuration=Release
 }
--- Regalo.Testing/Properties/AssemblyInfo.cs
+++ Regalo.Testing/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Regalo.Testing")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Regalo.Testing")]
