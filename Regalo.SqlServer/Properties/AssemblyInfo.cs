b696e8341810be4fe8a90fbb4432946f54fe0f24
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-11-06 23:56:22 +0000

    Regalo.SqlServer: Updated assemblyinfo.cs and project to use auto-versioning.

--- Regalo.SqlServer/Properties/AssemblyInfo.cs
+++ Regalo.SqlServer/Properties/AssemblyInfo.cs
@@ -6,7 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Regalo.EventSourcing.SqlServer")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Regalo.EventSourcing.SqlServer")]
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6eaafdfb-22ab-488e-ae27-4a00bb0e1bc8")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
