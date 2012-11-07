f8ba5edb389f707434d7e410085bcb086484a7b3
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2012-11-07 10:40:36 +0000

    Fixed PushPackage.ps1 script.

--- PushPackage.ps1
+++ PushPackage.ps1
@@ -1,8 +1,12 @@
+write-verbose "Preparing to push packages to nuget..."
+
+$scriptDir = $MyInvocation.MyCommand.Path | split-path
 $outputDir = "$scriptDir\BuildOutput"
+write-debug "outputDir = $outputDir"
 
 md $outputDir -f | out-null
 
-gci $outputDir -include *.nupkg | %{ 
+gci $outputDir -filter *.nupkg | %{ 
     Write-Host -ForegroundColor Green "$_"
-    nuget.exe push $_
+    nuget.exe push $_.FullName
 }
