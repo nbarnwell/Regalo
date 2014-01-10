a218ee679530cf1355895e29c9a9da1ac1c3c970
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2014-01-10 22:03:41 +0000

    Added package dependency on ObjectCompare to Testing project.

--- BuildPackage.ps1
+++ BuildPackage.ps1
@@ -3,7 +3,7 @@ $outputDir = "$scriptDir\BuildOutput"
 
 md $outputDir -f | out-null
 
-gci $scriptDir -include Regalo.Core.csproj,Regalo.RavenDB.csproj,Regalo.SqlServer.csproj,Regalo.Testing.csproj -recurse | %{ 
+gci $scriptDir -include Regalo.Core.csproj,Regalo.RavenDB.csproj,Regalo.SqlServer.csproj,Regalo.Testing.csproj,Regalo.ObjectCompare.csproj -recurse | %{ 
     Write-Host -ForegroundColor Green "$_"
     nuget.exe pack $_ -Build -Symbols -outputdirectory $outputDir -Properties Configuration=Release
 }
