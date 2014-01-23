c63ebda334c184eadcdd1e76076221cd978c398e
Neil Barnwell <mailbox@neilbarnwell.co.uk>
2014-01-23 17:42:26 +0000

    Impl'd PropertyComparisonIgnoreList for ignoring properties in ObjectCompare.

---
+++ Regalo.ObjectCompare.Tests.Unit/PropertyIgnoringTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Regalo.ObjectCompare.Tests.Unit
+{
+    [TestFixture]
+    public class PropertyIgnoringTests
+    {
+        [Test]
+        public void IgnoreSingleProperty()
+        {
+            var ignoreList = new PropertyComparisonIgnoreList();
+            ignoreList.Add<SimpleObject, string>(x => x.StringProperty1);
+
+            Assert.That(ignoreList.Contains(typeof(SimpleObject), "StringProperty1"), Is.True);
+        }
+    }
+
+    public class SimpleObject
+    {
+        public string StringProperty1 { get; set; }
+        public string StringProperty2 { get; set; }
+    }
+}
---
+++ Regalo.ObjectCompare/PropertyComparisonIgnoreList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Regalo.ObjectCompare
+{
+    public class PropertyComparisonIgnoreList
+    {
+        private readonly IDictionary<RuntimeTypeHandle, HashSet<string>> _ignored = new Dictionary<RuntimeTypeHandle, HashSet<string>>();
+
+        public void Add<T, TProperty>(Expression<Func<T, TProperty>> expression)
+        {
+            var me = expression.Body as MemberExpression;
+
+            if (me == null)
+            {
+                throw new ArgumentException(
+                    string.Format("The name of the property could not be established from the expression '{0}'", expression),
+                    "expression");
+            }
+
+            var type = typeof(T);
+            HashSet<string> propertiesIgnoredForType;
+            if (false == _ignored.TryGetValue(type.TypeHandle, out propertiesIgnoredForType))
+            {
+                propertiesIgnoredForType = new HashSet<string>();
+                _ignored.Add(type.TypeHandle, propertiesIgnoredForType);
+            }
+
+            if (false == propertiesIgnoredForType.Contains(me.Member.Name))
+            {
+                propertiesIgnoredForType.Add(me.Member.Name);
+            }
+        }
+
+        public bool Contains(Type type, string propertyName)
+        {
+            HashSet<string> propertiesIgnoredForType;
+            if (false == _ignored.TryGetValue(type.TypeHandle, out propertiesIgnoredForType))
+            {
+                return false;
+            }
+
+            return propertiesIgnoredForType.Contains(propertyName);
+        }
+    }
+}
